5e03386f846adef3b2689ffacd39c2bd22847888
Pranay Jain <pranayjain@ibm.com>
2024-09-13 00:53:33 +0530

    Added category response in get All Categories updated response

--- sb-ecom/src/main/java/com/ecommerce/project/payload/CategoryResponse.java
+++ sb-ecom/src/main/java/com/ecommerce/project/payload/CategoryResponse.java
@@ -16,11 +16,59 @@ import java.util.List;
 public class CategoryResponse {
     private List<CategoryDTO> content;
 
+    private Integer pageNumber;
+
+    private Integer pageSize;
+    private Long totalElements;
+
+    private Integer totalpages;
+    private Boolean lastPage;
+
     public List<CategoryDTO> getContent() {
         return content;
     }
 
     public void setContent(List<CategoryDTO> content) {
         this.content = content;
+    }
+
+    public Integer getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(Integer pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(Long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public Integer getTotalpages() {
+        return totalpages;
+    }
+
+    public void setTotalpages(Integer totalPages) {
+        this.totalpages = totalPages;
+    }
+
+    public Boolean getLastPage() {
+        return lastPage;
+    }
+
+    public void setLastPage(Boolean lastPage) {
+        this.lastPage = lastPage;
     }
 }
--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -40,6 +40,12 @@ public class CategoryServiceImpl implements CategoryService{
 
         CategoryResponse categoryResponse = new CategoryResponse();
         categoryResponse.setContent(categoryDTOS);
+        categoryResponse.setPageNumber(categoryPage.getNumber());
+        categoryResponse.setPageSize(categoryPage.getSize());
+        categoryResponse.setTotalElements(categoryPage.getTotalElements());
+        categoryResponse.setTotalpages(categoryPage.getTotalPages());
+        categoryResponse.setLastPage(categoryPage.isLast());
+
         return categoryResponse;
     }
 
