0e19665850c4105a9923a37c8d2e49ed434219bb
Pranay Jain <pranayjain@ibm.com>
2024-09-12 22:43:11 +0530

    CategoryDTO - For update query

--- sb-ecom/src/main/java/com/ecommerce/project/config/AppConfig.java
+++ sb-ecom/src/main/java/com/ecommerce/project/config/AppConfig.java
@@ -12,3 +12,9 @@ public class AppConfig {
         return new ModelMapper();
     }
 }
+
+
+/*
+* ModelMapper is a Java library that simplifies object mapping by automating the copying of data between two objects, typically of different classes.
+*  It is particularly useful when you have complex nested objects or when you need to map between DTOs (Data Transfer Objects) and domain models.
+* */
